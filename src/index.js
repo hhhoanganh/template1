88cce397c2b70bee41fce39e35256ae17e0134cc
hhhoanganh <101188848+hhhoanganh@users.noreply.github.com>
2024-05-04 21:04:41 +0700

    begin
    begin

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+import { ArgonControllerProvider } from "context";
+
+import PerfectScrollbar from "react-perfect-scrollbar";
+
+import "react-perfect-scrollbar/dist/css/styles.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter basename="/argon-dashboard-material-ui">
+    <ArgonControllerProvider>
+      <PerfectScrollbar>
+        <App />
+      </PerfectScrollbar>
+    </ArgonControllerProvider>
+  </BrowserRouter>
+);
